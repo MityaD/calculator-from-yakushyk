f7d2ab2892257f7a69918c9469c62b249097d309
Dima Yakushuk <yakushyk.dmitrii@gmail.com>
2021-07-22 00:34:46 +0300

    Check + StringInInt

--- src/com/company/calculator/Main1.java
+++ src/com/company/calculator/Main1.java
@@ -2,7 +2,7 @@ package com.company.calculator;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main1 {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
