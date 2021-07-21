431e2352140aec115a6c94a8a1476429855d57a2
Dima Yakushuk <yakushyk.dmitrii@gmail.com>
2021-07-22 00:40:57 +0300

    no Rinsk

--- src/com/company/calculator/Main.java
+++ src/com/company/calculator/Main.java
@@ -2,7 +2,7 @@ package com.company.calculator;
 
 import java.util.Scanner;
 
-public class Main1 {
+public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
